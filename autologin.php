d445eb6f5099bcc46d67a3e197d746e8eeddabe7
Shahin <61395685+ShahinZa@users.noreply.github.com>
2020-07-07 09:44:41 -0400

    Update autologin.php

--- autologin.php
+++ autologin.php
@@ -44,7 +44,7 @@ function autologin_init(){
 
 		echo "
 		<label>Password:</label>
-		<input type='password' name='password_al' placeholder=$upass />
+		<input type='password' name='password_al' placeholder='Password' />
 		";
 
 		echo "
