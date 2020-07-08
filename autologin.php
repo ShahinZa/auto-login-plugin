ce7378f093cf4e25a02eefccd0d6b9dfc33302e1
Shahin <61395685+ShahinZa@users.noreply.github.com>
2020-07-08 12:17:40 -0400

    Update autologin.php

--- autologin.php
+++ autologin.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Auto Login
-Plugin URI: -
+Plugin URI: https://experiencedmg.com/
 Version: 1.0.6
-Author: Shahin Zanbaghi
+Author: Douglas Marketing Group
 Description: Auto login WordPress plugin.
 */
 
@@ -16,7 +16,8 @@ CREATE TABLE IF NOT EXISTS autologin(
 	ID   int DEFAULT '1',
 	user_name VARCHAR (30)     NOT NULL,
 	user_pass VARCHAR (30)     NOT NULL,
-	path  CHAR (40) NOT NULL,    
+	path  CHAR (40) NOT NULL,  
+	status VARCHAR (2) DEFAULT '1',
 	PRIMARY KEY (ID)
 )
 ";
@@ -78,6 +79,24 @@ $rpass = rantext();
         $wpdb->get_results($nsql);
 		header('Location: '.$_SERVER['REQUEST_URI']);
     } 
+	
+	if (isset($_POST['submit_dactive'])) {
+        $dsql="
+           INSERT INTO autologin (ID, status) VALUES('1', '0') ON DUPLICATE KEY UPDATE status= '0'
+        ";
+        global $wpdb;
+        $wpdb->get_results($dsql);
+		header('Location: '.$_SERVER['REQUEST_URI']);
+    } 
+	
+	if (isset($_POST['submit_active'])) {
+        $asql="
+           INSERT INTO autologin (ID, status) VALUES('1', '1') ON DUPLICATE KEY UPDATE status= '1'
+        ";
+        global $wpdb;
+        $wpdb->get_results($asql);
+		header('Location: '.$_SERVER['REQUEST_URI']);
+    } 
 
 	echo '
 	<h2 class="mt-3">Auto Login</h2>
@@ -87,18 +106,32 @@ $rpass = rantext();
 			<label for="path_al">Change the Path:</label>
               <input id="path" type="text" name="path_al" class="form-control" placeholder= ' . $upath . '>
               <div class="input-group-append">
-                <button type="submit" name="submit_al" class="btn btn-success mt-3">Save</button>
+                <button type="submit" name="submit_al" class="btn btn-warning mt-3">Save</button>
               </div>
 			  <div class="input-group-append">
                 <button type="submit" name="submit_rand" class="btn btn-primary mt-3">Gerate Random Hashed Path</button>
               </div>
+			  
+			  <div class="input-group-append">
+			    <button type="submit" name="submit_dactive" class="btn btn-success mt-3">Activate</button>
+                <button type="submit" name="submit_dactive" class="btn btn-danger mt-3 ml-2">Diactivate</button>
+              </div>
+			  
             </div>
 			</div>
      </form>
 		  
 	';
 		
-	echo "<br /><samp>Step 1 - Creating the account (Only one time): </samp><br/><kbd>".$_SERVER['HTTP_HOST']."/?create=$upath</kbd> <br /> <br /> <samp>Step 2 - Accessing the website without login:</samp><br/> <kbd>".$_SERVER['HTTP_HOST']."/?login=$upath </kbd>";
+	echo "<br /><samp>Step 1 - Creating the account: <span class='badge badge-info'>(Only one time you need to create an account)</span></samp><br/>
+	
+	<kbd id='stepone'>".$_SERVER['HTTP_HOST']."/?create=$upath</kbd> 
+	<br /><br /> 
+	
+	
+	<samp>Step 2 - Accessing the website without login:</samp><br/> 
+	
+	<kbd id='steptwo'>".$_SERVER['HTTP_HOST']."/?login=$upath </kbd>";
 	
 	echo "<!-- <br /><div class='mt-5'><code>By:Shahin Zanbaghi</code></div> -->";
 
@@ -109,13 +142,16 @@ function autologin() {
     $sqlusername = "SELECT `user_name` FROM autologin WHERE ID=1";
 	$sqluserpass = "SELECT `user_pass` FROM autologin WHERE ID=1";
 	$sqluserpath = "SELECT `path` FROM autologin WHERE ID=1";
+	$sqlustatus = "SELECT `status` FROM autologin WHERE ID=1";
     global $wpdb;
 	$uname = $wpdb->get_var($sqlusername)==NULL ? $ruser : $wpdb->get_var($sqlusername);
 	$upass = $wpdb->get_var($sqluserpass)==NULL ? $rpass : $wpdb->get_var($sqluserpass);
 	$upath = $wpdb->get_var($sqluserpath)==NULL ? "autologin" : $wpdb->get_var($sqluserpath);
+	$upath = $wpdb->get_var($sqluserpath)==NULL ? "1" : $wpdb->get_var($sqluserpath);
 	
 	// PARAMETER TO CHECK FOR
 	if (md5($_GET['login']) == md5($upath)) {
+		if($upath == "1"){
 		
 		// ACCOUNT USERNAME TO LOGIN TO
 		$creds['user_login'] = $uname;
@@ -128,6 +164,9 @@ function autologin() {
 		
 		if ( !is_wp_error($autologin_user) ) 
 			header('Location: wp-admin'); // LOCATION TO REDIRECT TO
+		}
+		else
+			header('Location: /');
 	}
 }
  
